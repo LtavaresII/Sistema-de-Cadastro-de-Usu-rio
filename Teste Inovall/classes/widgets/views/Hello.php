41290b3b18148a835c3f7f6ba4c8b484533e69ea
LtavaresII <30603702+LtavaresII@users.noreply.github.com>
2022-10-16 18:43:13 -0300

    Delete Teste Inovall directory

--- Teste Inovall/classes/widgets/views/Hello.php
+++
@@ -1,5 +0,0 @@
-<h2><?= $message ?></h2>
-<p>wekiofdhawhadadadadadadhadhadahdahdddddddauuuuuuuuuuuuuuuudhadhadhuaiudhiuwhdauhdiauhdiahidwhaiwdhiawhdiahdwiahdiahdiahdiuahdiadhuiahdiahdiahdiahdiadhiahdiahdiahdad</p>
-
-<h2><?= $submessage ?></h2>
-<p>wekiofdhawhadadadadadadhadhadahdahdddddddauuuuuuuuuuuuuuuudhadhadhuaiudhiuwhdauhdiauhdiahidwhaiwdhiawhdiahdwiahdiahdiahdiuahdiadhuiahdiahdiahdiahdiadhiahdiahdiahdad</p>
